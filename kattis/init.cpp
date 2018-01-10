497e658f3fceb4163310e6aba6f1c41366f675f3
Hannes Kock <goffkock@gmail.com>
2018-01-10 23:05:05 +0100

    kitten complete

--- kattis/init.cpp
+++ kattis/init.cpp
@@ -1,10 +1,40 @@
+#include <stdio.h>
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <math.h>
 #include <map>
-
+#include <set>
+#include <sstream>
+#include <cstring>
 using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+
+#define REP(i, a, b) \
+	for (int i = int(a); i < int(b); i++)
+#define TRvi(c, it) \
+	for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) \
+	for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRmsi(c, it) \
+	for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+
+#define INF 2000000000 // 2 billion
+// If you need to recall how to use memset:
+#define MEMSET_INF 127 // about 2B
+#define MEMSET_HALF_INF 63 // about 1B
+//memset(dist, MEMSET_INF, sizeof dist); // useful to initialize shortest path distances
+//memset(dp_memo, -1, sizeof dp_memo); // useful to initialize DP memoization table
+//memset(arr, 0, sizeof arr); // useful to clear array of integers
+
+#define DFS_BLACK 1
+#define DFS_WHITE -1
 
 int main(void) {
 	int n;
--- kattis/kitten/kitten.cpp
+++ kattis/kitten/kitten.cpp
@@ -1,15 +1,85 @@
+#include <stdio.h>
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <math.h>
-#include <map>
-
+#include <sstream>
+#include <cstring>
 using namespace std;
 
-int main(void) {
-	int n;
-	cin >> n;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
 
+#define REP(i, a, b) \
+	for (int i = int(a); i < int(b); i++)
+#define TRvi(c, it) \
+	for (vi::iterator it = (c).begin(); it != (c).end(); it++)
+#define TRvii(c, it) \
+	for (vii::iterator it = (c).begin(); it != (c).end(); it++)
+
+vii AdjList[100];
+
+bool dfs(int branch, int toFind, vi& result) {
+	if(branch == toFind) {
+		result.push_back(branch);
+		return true;
+	}
+	TRvii (AdjList[branch], v) {
+		if(dfs(v->first, toFind, result)) {
+			result.push_back(branch);
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(void) {
+	int kittenAt;
+	string line;
+	bool first = true;
+	vi branches;
+	while(getline(cin, line)) {
+		istringstream l(line);
+		int branch;
+		l >> branch;
+		if(branch == -1) {
+			break;
+		}
+		if(first) {
+			kittenAt = branch;
+			first = false;
+			continue;
+		}
+		branches.push_back(branch);
+		int edgeTo;
+		vii adj;
+		ii p;
+		while(l >> edgeTo) {
+			p = make_pair(edgeTo, 0);
+			adj.push_back(p);
+		}
+		AdjList[branch] = adj;
+	}
+
+	int maxPathLength = 0;
+	vi maxPath;
+	REP(i, 0, branches.size()) {
+		vi result;
+		dfs(branches[i], kittenAt, result);
+		if(result.size() > maxPathLength) {
+			maxPathLength = result.size();
+			maxPath = result;
+		}
+	}
+
+	REP(j, 0, maxPath.size()) {
+		cout << maxPath[j];
+		if(j != maxPath.size() - 1) {
+			cout << " ";
+		}
+	}
 	cout << endl;
 
 	return 0;
