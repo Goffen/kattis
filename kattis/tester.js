beca4f7feee340736a6419b455f6eca26a981905
Hannes Kock <goffkock@gmail.com>
2018-01-24 18:23:59 +0100

    tester

--- kattis/tester.js
+++ kattis/tester.js
@@ -1,8 +1,5 @@
 var path = require('path');
 var fs = require('fs');
-//if(process.argv.length != 3) {
-//	process.exit(0);
-//}
 var dir = process.argv[2];
 
 function runProcess(input, output, indx) {
@@ -13,16 +10,17 @@ function runProcess(input, output, indx) {
 	child1.stdin.write(input);
 	child1.stdout.on('data', (data) => {
 		datas.push(data);
-	  	//console.log(`${indx}: ${data} ${data.toString() == 'yes\r\n'}`);
 	});
 	child1.on('close', function() {
 		console.log("-------- " + indx + " -------");
 		console.log(input.toString());
-		//var windowsOutput = output.toString().replace(/\n/g, '\r\n');
 		console.log('- expected -')
 		console.log(output.toString());
 		console.log('- output -')
       	var programOutput = datas.toString();
+      	if(process.platform === 'win32') {
+      		programOutput = programOutput.replace(/\r\n/g, '\n');
+      	}
 		console.log(programOutput);
 		console.log("lengths= ans " + output.length + " vs output " + programOutput.length);
 		console.log("equals=" + (output == programOutput));
@@ -33,16 +31,12 @@ function runProcess(input, output, indx) {
 
 fs.readdir(dir, function(err, files) {
 	files.sort();
-	var samples = 0;
 	for (var i = 0; i < files.length; i++) {
 		var file = files[i];
 		if(file.endsWith('.in')) {
-			var indx = parseInt(file.substring(0, file.indexOf('.')));
-			samples = indx;
+			var fileName = file.substr(0, file.length - 3);
+			runProcess(fs.readFileSync(path.join(dir, file)), fs.readFileSync(path.join(dir, fileName + ".ans")), i);
 		}
-	}
-	for (var i = 1; i <= samples; i++) {
-		runProcess(fs.readFileSync(path.join(dir, i + ".in")), fs.readFileSync(path.join(dir, i + ".ans")), i);
 	}
 })
 
