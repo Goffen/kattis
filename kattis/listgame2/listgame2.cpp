bf10fee8fec223ad698f6f510688c79f1a78f101
hannes <hannes@fisen.lan>
2018-01-29 10:26:50 +0100

    listgame2

--- kattis/listgame2/listgame2.cpp
+++ kattis/listgame2/listgame2.cpp
@@ -11,113 +11,122 @@ using namespace std;
 
 typedef long long ll;
 typedef vector<ll> vll;
+typedef vector<int> vi;
+#define SQRT10_15 31622777
+#define SQRT10_15_EXTRA 31722777
 
 #define REP(i, a, b) \
-	for (int i = int(a); i < int(b); i++)
+   for (int i = int(a); i < int(b); i++)
 
 /* Creates list of primes in [0..upperboud] */
 void sieve(vll& primes, ll upperbound) {
-	bitset<10000010> bs;
-	bs.reset(); 
-	bs.flip();
-	bs.set(0, false);
-	bs.set(1, false); // 0, 1 is not prime
-	for(ll i = 2; i <= upperbound + 1; i++) {
-		if(bs.test((size_t)i)) {
-			// cross out multiples starting from i*i
-			for(ll j = i*i; j <= upperbound + 1; j += i) {
-				bs.set((size_t)j, false);
-			}
-			primes.push_back((ll)i);
-		}
-	}
+   bitset<SQRT10_15_EXTRA> bs;
+   bs.reset(); 
+   bs.flip();
+   bs.set(0, false);
+   bs.set(1, false); // 0, 1 is not prime
+   for(ll i = 2; i <= upperbound + 1; i++) {
+      if(bs.test((size_t)i)) {
+         // cross out multiples starting from i*i
+         for(ll j = i*i; j <= upperbound + 1; j += i) {
+            bs.set((size_t)j, false);
+         }
+         primes.push_back((ll)i);
+      }
+   }
 }
-vll primeFactors(ll n, vll& primes) {
-	vll factors;
-	int nrFactors = 0;
-	int indx = 0;
-	ll pf = 2;
 
-	while(n != 1 && (pf * pf <= n)) {
-		while(n % pf == 0) {
-			n /= pf;
-			factors.push_back(pf);
-		}
-		pf = primes[++indx];
-	}
-	if(n != 1) {
-		factors.push_back(n);
-	}
-	return factors;
+vll primeFactors(ll n, vll& primes) {
+   vll factors;
+   int nrFactors = 0;
+   int indx = 0;
+   ll pf = 2;
+
+   while(n != 1 && (pf * pf <= n)) {
+      while(n % pf == 0) {
+         n /= pf;
+         factors.push_back(pf);
+      }
+      pf = primes[++indx];
+   }
+   if(n != 1) {
+      factors.push_back(n);
+   }
+   return factors;
 }
 
 int addSum(int n) {
-	return ((n * (n+1)) / 2);
+   return ((n * (n+1)) / 2);
+}
+
+int dfsFactorize(ll n, ll sum, vi& multiples, int st, int depth) {
+   if(sum > n) { return -1;}
+   if(sum == n) { 
+      return depth;
+   }
+   int m = -1;
+   int maxMul = -1;
+   REP(i, st, multiples.size()) {
+      int r = dfsFactorize(n, sum * multiples[i], multiples, i + 1, depth + 1);
+      if(r != -1 && r > m) {
+      	maxMul = multiples[i];
+      }
+      m = max(m, r);
+   }
+   /*if(maxMul != -1) {
+   	cout << m << ", " << maxMul << endl;
+   }*/
+   return m;
+}
+/*
+   Mål 1: 2^5 + 3^2 borde bli 2*3*6*8=4
+   Mål 2: 2^40 borde bli 2*4*8*16*32*64*128*4096=8
+ */
+int primeFactorsBfs(ll n) {
+   vi factors;
+   vi multiple;
+   REP(i,1,20) { multiple.push_back(pow(2, i));};
+   REP(i,1,10) { multiple.push_back(pow(3, i));};
+   REP(i,1,10) { multiple.push_back(pow(5, i));};
+   REP(i,1,10) { multiple.push_back(3*2*i);};
+   sort(multiple.begin(), multiple.end());
+   return dfsFactorize(n, 1, multiple, 0, 0);
 }
 
 int main(void) {
-	ll x;
-	cin >> x;
-	vll primes;
-	sieve(primes, pow(10,7));
-	vll factors = primeFactors(x, primes);
-	vll uniqueFactors;
-	map<ll, int> nrOfUniques;
-	REP(i,0, factors.size()) { 
-		int uniques = 1;
-		ll fact = factors[i];
-		REP(j, 0, factors.size()) {
-			if(i != j && fact == factors[j]) {
-				uniques++;
-			}
-		}
-		nrOfUniques[fact] = uniques;
-	}
-	for(map<ll,int>::iterator it = nrOfUniques.begin(); it != nrOfUniques.end(); it++) {
-		ll fact = it->first;
-		int uniques = it->second;
-		
-		if(uniques > 1) {
-			int pows = 1;
-			int nrOfFactSums = 0;
-			while(uniques - nrOfFactSums > 0) {
-				nrOfFactSums = addSum(++pows);
-			}
-			/*cout << "uniques: " << uniques << endl;
-			cout << "nrOfFactSums: " << nrOfFactSums << endl;
-			cout << "pows: " << pows << endl;
-			cout << "fact: " << fact << endl;*/
-			if(uniques - nrOfFactSums < 0) {
-				int powToMany = uniques - addSum(pows - 2);
-				REP(k, 1, pows - 1) {
-					uniqueFactors.push_back(pow(fact, k));
-				}
-				uniqueFactors.push_back(pow(fact, powToMany));
-			}
-			else if(uniques - nrOfFactSums == 0) {
-				REP(k, 1, pows+1) {
-					uniqueFactors.push_back(pow(fact, k));
-				}
-			}
-		}
-		else {
-			uniqueFactors.push_back(fact);
-		}
-	}
+   ll x;
+   cin >> x;
+   vll primes;
+   sieve(primes, round(sqrt(x)));
+   vll factors = primeFactors(x, primes);
+   vll uniqueFactors;
+   map<ll, int> nrOfUniques;
+   REP(i,0, factors.size()) { 
+      int uniques = 1;
+      ll fact = factors[i];
+      REP(j, 0, factors.size()) {
+         if(i != j && fact == factors[j]) {
+            uniques++;
+         }
+      }
+      cout << fact << endl;
+      nrOfUniques[fact] = uniques;
+   }
+   //cout << "---- " << endl;
+   ll prodOfUniques = 1;
+   int notDuplicates = 0;
+   for(map<ll,int>::iterator it = nrOfUniques.begin(); it != nrOfUniques.end(); it++) {
+      ll fact = it->first;
+      int uniques = it->second;
+      if(uniques > 1) {
+         prodOfUniques *= pow(fact, uniques);
+      }
+      else {
+         notDuplicates++;
+      }
+   }
+   int duplicateFactorized = primeFactorsBfs(prodOfUniques);
+   cout << (duplicateFactorized + notDuplicates) << endl;
 
-	REP(i, 0, uniqueFactors.size()) {
-		cout << uniqueFactors[i] << endl;
-	}
-	
-	//cout << uniqueFactors.size() << endl;
-
-	return 0;
+   return 0;
 }
-
-/*
-
-2^40 -> 1099511627776
-
-2^5 * 2^2 * 2^3 * 2^4
-
-*/
